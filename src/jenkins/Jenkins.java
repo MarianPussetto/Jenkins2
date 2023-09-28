784974cea3e9afa26c033298ec70989a24b7e36e
MarianPussetto <marianpussetto@gmail.com>
2023-09-28 20:20:45 -0300

    hola mundoooo

--- src/jenkins/Jenkins.java
+++ src/jenkins/Jenkins.java
@@ -7,7 +7,7 @@ public class Jenkins {
     public static void main(String[] args) {
         
         System.out.println("A commit has been made.");
-        System.out.println("HOLA MUNDO");
+        System.out.println("HOLA MUNDOooooo");
 
        
     }
