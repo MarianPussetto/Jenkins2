fccb517d7c1219e59f6b2123224c092021a5b0f5
MarianPussetto <marianpussetto@gmail.com>
2023-09-28 20:04:56 -0300

    nuevoo

--- src/jenkins/Jenkins.java
+++ src/jenkins/Jenkins.java
@@ -8,7 +8,7 @@ public class Jenkins {
         
         System.out.println("A commit has been made.");
         System.out.println("I'm introducing a new change");
-        System.out.println("Último cambio");
+        System.out.println("Cambioo");
        
     }
 
