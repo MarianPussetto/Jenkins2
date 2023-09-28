0c79d0264fe5ef30c0fa1a991c583519ec4f9a5a
MarianPussetto <marianpussetto@gmail.com>
2023-09-28 20:18:24 -0300

    hola mundo

--- src/jenkins/Jenkins.java
+++ src/jenkins/Jenkins.java
@@ -7,8 +7,8 @@ public class Jenkins {
     public static void main(String[] args) {
         
         System.out.println("A commit has been made.");
-        System.out.println("I'm introducing a new change");
-        System.out.println("Cambioo");
+        System.out.println("HOLA MUNDO");
+
        
     }
 
