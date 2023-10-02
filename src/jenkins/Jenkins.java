9849b6237755d52fd273ffccd90658b3c5b46012
MarianPussetto <marianpussetto@gmail.com>
2023-10-02 09:46:27 -0300

    adhiero lo nuevo

--- src/jenkins/Jenkins.java
+++ src/jenkins/Jenkins.java
@@ -7,8 +7,8 @@ public class Jenkins {
     public static void main(String[] args) {
         
         System.out.println("A commit has been made.");
-        System.out.println("HOLA MUNDOooooo");
-        System.out.println("lalala");
+        System.out.println("Nueva prueba");
+
 
        
     }
