8fb3c76215417c32108a78c97fe2b3b258677c80
MarianPussetto <marianpussetto@gmail.com>
2023-10-02 20:47:47 -0300

    probando Ngrok

--- src/jenkins/Jenkins.java
+++ src/jenkins/Jenkins.java
@@ -7,11 +7,8 @@ public class Jenkins {
     public static void main(String[] args) {
         
         System.out.println("A commit has been made.");
-        System.out.println("Nueva prueba");
-        System.out.println("nuevo");
+        System.out.println("probando conexion con Ngrok");
 
-
-       
     }
 
 }
