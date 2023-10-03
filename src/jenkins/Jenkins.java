18f3a03272cb3baf56bb08a1db98f83b695248db
MarianPussetto <marianpussetto@gmail.com>
2023-10-02 21:34:13 -0300

    hola

--- src/jenkins/Jenkins.java
+++ src/jenkins/Jenkins.java
@@ -7,9 +7,8 @@ public class Jenkins {
     public static void main(String[] args) {
         
         System.out.println("A commit has been made.");
-        System.out.println("probando conexion con Ngrok");
-        System.out.println("no andaaaaAAAA");
-        System.out.println("LPMMMM");
+        System.out.println("probando conexion con Ngrok otra vez");
+ 
 
     }
 
